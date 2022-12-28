878f2722a4d5339aaef327c4c195919f00e1e057
Marcus Ahlfors <marcus.ahlfors@nemesys.fi>
2022-12-28 15:17:16 +0200

    Make runonce.sh current  (eg. credential-helper)

--- runonce.sh
+++ runonce.sh
@@ -3,6 +3,9 @@
 # Perform tasks at controller pod startup
 echo "Runonce started";
 
+# Insert accepted ssh key(s)
+cat /etc/ssh/internal_ssh_host_rsa.pub >> /root/.ssh/authorized_keys;
+
 cd /app;
 
 # Seems that this is first run in devel instance
@@ -11,26 +14,28 @@ if [ ! "$(ls -A /app)" ]; then
 
   echo "Empty /app, assuming development instance setup was intended"
   #tar zxf /var/lib/diploi-app.tar.gz  -C /
+  mkdir -p /root-persist/.vscode-server;
+  touch /root-persist/.bash_history;
+  touch /root-persist/.gitconfig;
 
   git init;
+  git config credential.helper '!diploi-credential-helper';
   git remote add --fetch origin $REPOSITORY_URL;
   git checkout -f $REPOSITORY_BRANCH;
   git remote set-url origin "$REPOSITORY_URL";
-  git config --global user.email "$GIT_USER_EMAIL";
-  git config --global user.name "$GIT_USER_NAME";
+  git config --unset credential.helper;
+  
+  npm install;
 
 fi
 
 # Update internal ca certificate
 update-ca-certificates
 
-# Insert accepted ssh key(s)
-cat /etc/ssh/internal_ssh_host_rsa.pub >> /root/.ssh/authorized_keys;
-
 # Make all special env variables available in ssh also (ssh will wipe out env by default)
 env >> /etc/environment
 
-# Now that everything is initialized, start the application
+# Now that everything is initialized, start all services
 supervisorctl start app
 
 echo "Runonce done";
