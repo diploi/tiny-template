f31365b4cb54e3f804e2e7553fe46db7bcc1cba5
VP <ville-pekka.lahti@nemesys.fi>
2023-02-20 10:44:34 +0200

    Transfer

---
+++ README.md
@@ -0,0 +1,3 @@
+# Diploi Tiny template
+
+A minimal Node 18 web application template just to showcase what is required to get a working app running.
---
+++ runonce.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Perform tasks at controller pod startup
+echo "Runonce started";
+
+# Insert accepted ssh key(s)
+cat /etc/ssh/internal_ssh_host_rsa.pub >> /root/.ssh/authorized_keys;
+
+# Update internal ca certificate
+update-ca-certificates
+
+# Make all special env variables available in ssh also (ssh will wipe out env by default)
+env >> /etc/environment
+
+cd /app;
+
+# Seems that this is first run in devel instance
+# Intialize persistant storage
+if [ ! "$(ls -A /app)" ]; then
+
+  echo "Empty /app, assuming development instance setup was intended"
+  #tar zxf /var/lib/diploi-app.tar.gz  -C /
+  mkdir -p /root-persist/.vscode-server;
+  touch /root-persist/.bash_history;
+  touch /root-persist/.gitconfig;
+
+  git config init.defaultBranch main
+  git init;
+  git config credential.helper '!diploi-credential-helper';
+  git remote add --fetch origin $REPOSITORY_URL;
+  git checkout -f $REPOSITORY_BRANCH;
+  git remote set-url origin "$REPOSITORY_URL";
+  git config --unset credential.helper;
+  
+  npm install;
+
+fi
+
+# Now that everything is initialized, start all services
+supervisorctl start app
+
+echo "Runonce done";
+
+exit 0;
